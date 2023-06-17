52ba2e828cb03990c60b22db7cacd6e556a6bf96
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-06-17 14:08:32 -0700

    Added Review model

---
+++ server/models/Review.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const reviewSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true, // Added index for querying reviews by user
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+      index: true, // Added index to improve query performance when searching for reviews of a particular product
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      required: true,
+      maxlength: 1000,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Review = model("Review", reviewSchema);
+module.exports = Review;
