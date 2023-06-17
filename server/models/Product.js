688f96d1842efb1d7c6d4113aa91aa4135363847
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-06-16 18:07:47 -0700

    Added timestamps to the product model

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,48 +1,51 @@
 const { Schema, model, Types } = require("mongoose");
 const validator = require("validator");
 
-const productSchema = new Schema({
-  title: {
-    type: String,
-    required: [true, "Product name is required"],
-    maxlength: [150, "Title cannot exceed 150 characters"],
-  },
-  description: {
-    type: String,
-    required: [true, "Product description is required"],
-    maxlength: [2000, "Description cannot exceed 2000 characters"],
-  },
-  price: {
-    type: Number,
-    required: [true, "Product price is required"],
-    validate: {
-      validator: (value) => value >= 0,
-      message: "Price must be a non-negative value",
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Product name is required"],
+      maxlength: [150, "Title cannot exceed 150 characters"],
     },
-  },
-  image: {
-    type: String,
-    validate: {
-      validator: (value) => validator.isURL(value),
-      message: "Invalid image URL",
+    description: {
+      type: String,
+      required: [true, "Product description is required"],
+      maxlength: [2000, "Description cannot exceed 2000 characters"],
     },
-  },
-  category: {
-    type: Schema.Types.ObjectId,
-    ref: "Category",
-    required: [true, "Product category is required"],
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
+    price: {
+      type: Number,
+      required: [true, "Product price is required"],
+      validate: {
+        validator: (value) => value >= 0,
+        message: "Price must be a non-negative value",
+      },
+    },
+    image: {
+      type: String,
+      validate: {
+        validator: (value) => validator.isURL(value),
+        message: "Invalid image URL",
+      },
+    },
+    category: {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: "Category",
+      required: [true, "Product category is required"],
     },
-  ],
-});
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 // Pre-save middleware
 productSchema.pre("save", async function (next) {
